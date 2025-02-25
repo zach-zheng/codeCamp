1a5811673fa711475c54bf202357bd5e5b5d74e9
zach-zheng <zhikui.cn@msn.com>
2025-02-25 20:54:03 +0800

    add Math.random

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeaponIndex].power;
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
 }
 
 function dodge() {
